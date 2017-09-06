b793402ca6efac331196779d9cd4badb2dacf5e0
Ratnasetya <ratnasetya209@gmail.com>
2017-09-06 12:42:07 +0700

    Progessing done!

---
+++ assets/js/app.js
@@ -0,0 +1,126 @@
+$("#q10").click(function() {
+    $("button").removeAttr("disabled");
+    $("button").click(function() {
+        var result = confirm('Form diklik');
+        if (result) {
+            alert('Terima kasih telah mengisi survey website BP2GAKI. Kami berharap dengan kontribusi Anda dalam mengisi survey ini, kami dapat memperbaiki dan meningkatkan pelayanan melalui website BP2GAKI');
+        } else {
+            alert("Oke, ada yang perlu diganti?");
+        }
+    });
+});
+$(function() {
+    $('.radio-sonar').click(function() {
+        $(".next").removeClass('d-none');
+        $(".next").addClass('d-block');
+    });
+});
+
+function optionsController(event) {
+    if (event.value == 'lainnya') {
+        $("#validationServer06").removeClass('d-none');
+        $("#validationServer06").addClass('d-block');
+    } else {
+        $("#validationServer06").removeClass('d-block');
+        $("#validationServer06").addClass('d-none');
+    }
+}
+
+$('.dropdown-menu a.dropdown-toggle').on('mouseenter', function(e) {
+    if (!$(this).next().hasClass('show')) {
+        $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+    }
+    var $subMenu = $(this).next(".dropdown-menu");
+    $subMenu.toggleClass('show');
+
+
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+        $('.dropdown-submenu .show').removeClass("show");
+    });
+
+
+    return false;
+});
+
+$('body').on('mouseenter mouseleave', '.dropdown', function(e) {
+    var _d = $(e.target).closest('.dropdown');
+    _d.addClass('show');
+    setTimeout(function() {
+        _d[_d.is(':hover') ? 'addClass' : 'removeClass']('show');
+    }, 300);
+});
+
+function responseSurveyFirst() {
+    return window.location.href = 'survey-pengunjung.html';
+}
+
+var pageLocation = '/survey-pengunjung.html';
+var halamanException = pageLocation;
+
+if (location.pathname == halamanException) {
+    $('#modalSurvey').modal('hide');
+} else {
+
+    setTimeout(function() {
+        $('#modalSurvey').modal('show');
+    }, 5000);
+}
+
+$(window).on('hashchange', function(e) {
+    getLastURI(location.href.substr(location.href.lastIndexOf('/') + 13));
+});
+
+function getLastURI(LastUri) {
+    let LastIndex = LastUri;
+
+    switch (LastIndex) {
+        case '#sejarah':
+            $('.if-sejarah').show();
+            $('.if-kepala-dari-masa-ke-masa').hide();
+            $('.if-visi-misi').hide();
+            $('.if-struktur-organisasi').hide();
+            $('.if-dasar-hukum').hide();
+            $('.if-sumber-daya-manusia').hide();
+            break;
+        case '#visi-misi':
+            $('.if-visi-misi').show();
+            $('.if-kepala-dari-masa-ke-masa').hide();
+            $('.if-sejarah').hide();
+            $('.if-struktur-organisasi').hide();
+            $('.if-dasar-hukum').hide();
+            $('.if-sumber-daya-manusia').hide();
+            break;
+        case '#struktur-organisasi':
+            $('.if-struktur-organisasi').show();
+            $('.if-kepala-dari-masa-ke-masa').hide();
+            $('.if-sejarah').hide();
+            $('.if-visi-misi').hide();
+            $('.if-dasar-hukum').hide();
+            $('.if-sumber-daya-manusia').hide();
+            break;
+        case '#dasar-hukum':
+            $('.if-dasar-hukum').show();
+            $('.if-kepala-dari-masa-ke-masa').hide();
+            $('.if-sejarah').hide();
+            $('.if-visi-misi').hide();
+            $('.if-struktur-organisasi').hide();
+            $('.if-sumber-daya-manusia').hide();
+            break;
+        case '#kepala-dari-masa-ke-masa':
+            $('.if-dasar-hukum').hide();
+            $('.if-kepala-dari-masa-ke-masa').show();
+            $('.if-sejarah').hide();
+            $('.if-visi-misi').hide();
+            $('.if-struktur-organisasi').hide();
+            $('.if-sumber-daya-manusia').hide();
+            break;
+        default:
+            $('.if-sumber-daya-manusia').show();
+            $('.if-kepala-dari-masa-ke-masa').hide();
+            $('.if-visi-misi').hide();
+            $('.if-struktur-organisasi').hide();
+            $('.if-dasar-hukum').hide();
+            $('.if-sejarah').hide();
+            break;
+    }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-const gulp        = require('gulp');
+const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-const sass        = require('gulp-sass');
+const sass = require('gulp-sass');
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'assets/scss/*.scss'])
         .pipe(sass())
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("assets/css"))
         .pipe(browserSync.stream());
 });
 
-// Move JS Files to src/js
+// Move JS Files to assets/js
 gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest("src/js"))
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(gulp.dest("assets/js"))
         .pipe(browserSync.stream());
 });
 
@@ -21,23 +21,23 @@ gulp.task('js', function() {
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "./src"  
+        server: "./"
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'assets/scss/*.scss'], ['sass']);
+    gulp.watch("./**.html").on('change', browserSync.reload);
 });
 
-// Move Fonts to src/fonts
+// Move Fonts to assets/fonts
 gulp.task('fonts', function() {
-  return gulp.src('node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest('src/fonts'))
+    return gulp.src('node_modules/font-awesome/fonts/*')
+        .pipe(gulp.dest('assets/fonts'))
 })
 
-// Move Font Awesome CSS to src/css
+// Move Font Awesome CSS to assets/css
 gulp.task('fa', function() {
-  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-    .pipe(gulp.dest('src/css'))
+    return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+        .pipe(gulp.dest('assets/css'))
 })
 
-gulp.task('default', ['js','serve', 'fa', 'fonts']);
+gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
